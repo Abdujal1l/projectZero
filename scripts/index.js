2db85a8c52d761d01b96388dfe2e34ed0d5a8859
abdujalil <olimjonov309@gmail.com>
2022-10-25 19:17:39 +0200

    personalMovieDB

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,20 @@
 'use strict';
-let isChecked = true ,
-    isClosed = false ;
+const numberOfFilms = +prompt('сколько фильмов вы уже посмотрели', '');
 
-console.log(!isChecked || isClosed);
+const personalMovieDB = {
+    count: numberOfFilms ,
+    movies: {} ,
+    actors: {} ,
+    genres: [] ,
+    privat: false 
+
+};
+
+const a = prompt('one of the last movies , you have watched', ''),
+      b = prompt('how would you rate it in the scale of 0 to 10 ', ''),
+      c = prompt('one of the last movies , you have watched', ''),
+      d = prompt('how would you rate it in the scale of 0 to 10 ', '');
+
+personalMovieDB.movies[a] = b ;
+personalMovieDB.movies[c] = d ;
+console.log(personalMovieDB);
