0081d6abdb1be4866662ba252a137a0931eb14eb
abdujalil <olimjonov309@gmail.com>
2022-12-26 21:07:25 +0100

    destruction of objects

---
+++ scripts/destructions.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const options = {
+    name: 'test' ,
+    width: 1024 ,
+    height: 1024 ,
+    colors: {
+        border: 'black' ,
+        bg: 'red'
+    } ,
+    makeTest: function() {
+        console.log('test');
+    }
+}; 
+
+const {border, bg} = options.colors;
+console.log(border, bg);
+
+
+options.makeTest();
+console.log(options);
+console.log(options.name);
+
+delete options.name    ;
+
+console.log(options);
+
+for (let key in options) {
+    if(typeof(options[key]) === 'object') {
+        for (let i in options[key]) {
+            console.log(`${i} , ${options[key] [i]}`);} 
+         }else {
+            console.log(`свойство  ${key} имеет значение ${options[key]} `);
+        }
+    }
+
+let counter = 0;
+for(let i in options){
+    counter++;
+}
+console.log(counter);
+
+console.log(Object.keys(options));
+console.log(Object.keys(options).length);
